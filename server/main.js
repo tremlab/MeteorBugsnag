24ccb2e3aa68c72841395fa7f73d1a8511ef2014
tremlab <rbalmert@gmail.com>
2018-04-23 15:21:36 -0700

    added bugsnag-node to server side.  Errors reporting, but stacktrace is unhelpful :(

--- server/main.js
+++ server/main.js
@@ -2,4 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  var bugsnag = require("bugsnag");
+
+  bugsnag.register("33cf7f73bd4995905ad84290bd54df25");
+  bugsnag.notify(new Error("Non-fatal"));
 });
